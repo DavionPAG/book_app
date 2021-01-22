764f8edc797fbe61ef428ac547f233d3606b6da8
santorsm <matt.santorsola@gmail.com>
2021-01-21 20:44:22 -0800

    lab12-card01

---
+++ data/seed.sql
@@ -0,0 +1,6 @@
+INSERT into books (
+  author,
+  title,
+  image_url,
+  description
+) VALUES ('McBoatface, Boaty','Call me Ishmael', 'https://ichef.bbci.co.uk/news/976/cpsprodpb/5CD0/production/_88806732_shipanimation.jpg', 'Name that ship');
